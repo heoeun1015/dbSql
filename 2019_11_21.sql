fc61b478e6f6615787ce74affc4136a103a0842b
heoeunChoi <he4615@gmail.com>
2019-11-22 17:21:11 +0900

    git kraken 설치 / 사용법 달력 작성 쿼리 수정내용 안내
    계층쿼리 실습
    
    계층 쿼리 특수함수
    .CONNECT_BY_ROOT(col)
    .SYS_CONNECT_BY_PATH(col, 구분자)
    .CONNECT_BY_ISLEAF
    
    분석함수 개요
    
    emp테이블에서 자신보다 급여가 한단계 낮은 사람의 급여를
    새로운 컬럼 값으로 생성하기 (인라인뷰 / 조인)

--- 2019_11_21.sql
+++ 2019_11_21.sql
@@ -109,8 +109,7 @@ CONNECT BY LEVEL <= 35;
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
+------------------------------------------------------------------------------------------------------------
  create table sales as 
 select to_date('2019-01-03', 'yyyy-MM-dd') dt, 500 sales from dual union all
 select to_date('2019-01-15', 'yyyy-MM-dd') dt, 700 sales from dual union all
@@ -122,7 +121,7 @@ select to_date('2019-05-11', 'yyyy-MM-dd') dt, 150 sales from dual union all
 select to_date('2019-05-30', 'yyyy-MM-dd') dt, 100 sales from dual union all
 select to_date('2019-06-22', 'yyyy-MM-dd') dt, 1400 sales from dual union all
 select to_date('2019-06-27', 'yyyy-MM-dd') dt, 1300 sales from dual;
- 
+------------------------------------------------------------------------------------------------------------ 
  
  
 SELECT *
@@ -152,6 +151,58 @@ GROUP BY TO_CHAR(dt, 'MM');
 -- null 은 group by에서는 계산이 되지 않는다.
 -- min을 쓰는게 좋다. 최솟값이 데이터를 절약하기 좋음.
 
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+------------------------------------------------------------------------------------------------------------
+create table dept_h (
+    deptcd varchar2(20) primary key ,
+    deptnm varchar2(40) not null,
+    p_deptcd varchar2(20),
+    
+    CONSTRAINT fk_dept_h_to_dept_h FOREIGN KEY
+    (p_deptcd) REFERENCES  dept_h (deptcd) 
+);
+
+insert into dept_h values ('dept0', 'XX회사', '');
+insert into dept_h values ('dept0_00', '디자인부', 'dept0');
+insert into dept_h values ('dept0_01', '정보기획부', 'dept0');
+insert into dept_h values ('dept0_02', '정보시스템부', 'dept0');
+insert into dept_h values ('dept0_00_0', '디자인팀', 'dept0_00');
+insert into dept_h values ('dept0_01_0', '기획팀', 'dept0_01');
+insert into dept_h values ('dept0_02_0', '개발1팀', 'dept0_02');
+insert into dept_h values ('dept0_02_1', '개발2팀', 'dept0_02');
+insert into dept_h values ('dept0_00_0_0', '기획파트', 'dept0_01_0');
+commit;
+------------------------------------------------------------------------------------------------------------
+
+
+
+
+-- 계층 쿼리
+-- START WITH : 계층의 시작 부분을 정의
+-- CONNECT BY : 계층간 연결 조건을 정의
+
+-- 하향식 계층 쿼리 (가장 최상위 조직에서부터 모든 조직을 탐색)
+
+SELECT *
+FROM dept_h;
+
+SELECT dept_h.*, LEVEL, LEVEL * 4, RPAD(' ', (LEVEL - 1) * 4, ' ') || dept_h.deptnm pad
+FROM dept_h
+START WITH deptcd = 'dept0'        -- START WITH p_deptcd IS NULL
+CONNECT BY PRIOR deptcd = p_deptcd;     -- PRIOR 현재 읽은 데이터 (XX 회사)
+
+-- ■ 계층쿼리 (실습 h_3) ---------------------------------------------------------------------------------------------------------------------------
+-- 디자인팀에서 시작하는 상향식 계층 쿼리를 작성하세요.
+SELECT deptcd, RPAD(' ', (LEVEL - 1) * 7, ' ') || dept_h.deptnm deptnm, p_deptcd
+FROM dept_h
+START WITH deptcd = 'dept0_02'
+CONNECT BY PRIOR deptcd = p_deptcd;
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
